896ba7cb52f1fb88642b7be4e62a5387e8d86661
a.papok <a.papok@freedom-finance.tech>
2023-10-07 18:58:39 +0200

    #HC-1 Fix comments.

--- src/main/java/com/healthcar/crm/ApplicationStarter.java
+++ src/main/java/com/healthcar/crm/ApplicationStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthCarCrmStarter {
+public class ApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthCarCrmStarter.class, args);
+		SpringApplication.run(ApplicationStarter.class, args);
 	}
 
 }
--- src/test/java/com/healthcar/crm/ApplicationStarterTests.java
+++ src/test/java/com/healthcar/crm/ApplicationStarterTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HealthCarCrmStarterTests {
+class ApplicationStarterTests {
 
 	@Test
 	void contextLoads() {
