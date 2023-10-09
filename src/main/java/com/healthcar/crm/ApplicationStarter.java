9e18d07c6ed12e49ee4d2497494a41616b07cb35
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2023-10-09 15:59:50 +0200

    fixes due to checkstyle warnings

--- src/main/java/com/healthcar/crm/ApplicationStarter.java
+++ src/main/java/com/healthcar/crm/ApplicationStarter.java
@@ -3,11 +3,17 @@ package com.healthcar.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The `ApplicationStarter` class is the entry point for the application.
+ * It includes a `main` method to launch the application using Spring Boot's `SpringApplication.run` method.
+ *
+ * @see org.springframework.boot.SpringApplication
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ */
 @SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
 public class ApplicationStarter {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStarter.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
 }
