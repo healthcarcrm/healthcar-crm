98caaf163207b1e89e57780d1436d69c6f5ad2e3
healthcarcrm <147187250+healthcarcrm@users.noreply.github.com>
2023-12-03 20:57:10 +0300

    [HC-3] checkstyle plugin implementation (#4)
    Co-authored-by: Andrey Shipulya <andrey.shipulya@vizor-games.com>

--- src/main/java/com/healthcar/crm/ApplicationStarter.java
+++ src/main/java/com/healthcar/crm/ApplicationStarter.java
@@ -3,11 +3,16 @@ package com.healthcar.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The `ApplicationStarter` class is the entry point for the application.
+ * It includes a `main` method to launch the application using Spring Boot's `SpringApplication.run` method.
+ *
+ * @see org.springframework.boot.SpringApplication
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ */
 @SpringBootApplication
 public class ApplicationStarter {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStarter.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
 }
--- src/main/java/com/healthcar/crm/controller/CrmController.java
+++ src/main/java/com/healthcar/crm/controller/CrmController.java
@@ -3,9 +3,17 @@ package com.healthcar.crm.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Base controller of the app.
+ */
 @RestController
 public class CrmController {
 
+    /**
+     * Test method.
+     *
+     * @return string for test info
+     */
     @GetMapping("/test")
     public String test() {
         return "test controller response";
